0d2fe59bf638923ec0a66b96eaae672519ba1cd3
AzellaStarLit <74412941+AzellaStarLit@users.noreply.github.com>
2025-06-21 01:12:47 +0800

    updated Utilities.h

--- HW1/HW1/Utilities.h
+++ HW1/HW1/Utilities.h
@@ -1,12 +1,15 @@
 #pragma once
 #define UTILITIES_H
+#include "Instructions.h"
+#include <string>
 
 void print_header();
 void clear();
 void initialize();
-void scheduler_test();
+void scheduler_start();
 void scheduler_stop();
 void report_util();
 void clear();
 void exit_program();
 
+std::string generate_instruction(const Instructions& tmpl, const std::string& processName);
